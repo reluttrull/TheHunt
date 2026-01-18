4548fb94b228b844f7754f46137a1b9ab724136f
Ryan Luttrull <reluttrull@gmail.com>
2026-01-18 16:55:59 -0600

    Start displaying markers for nearby places (no filtering yet)

--- TheHunt.client/src/app/places/nearby-places-map/nearby-places-map.ts
+++ TheHunt.client/src/app/places/nearby-places-map/nearby-places-map.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, inject, signal } from '@angular/core';
 import { LeafletDirective, LeafletLayerDirective } from '@bluehalo/ngx-leaflet';
-import { tileLayer, latLng, circle } from 'leaflet';
+import * as L from 'leaflet';
 import { PlaceService } from '../place.service';
 import { PlaceResponse } from '../interfaces';
 import { LatLong, getLocation } from '../../locations/utils';
@@ -17,19 +17,47 @@ export class NearbyPlacesMap implements OnInit {
   latitude: number | undefined;
   longitude: number | undefined;
 
-  options = {
-    layers: [tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })], 
-    zoom: 15, 
-    center: latLng(0,0)
-  };
-  layer = circle([0,0], { radius: 0});
+  options: L.MapOptions | undefined;
+  layer = L.circle([0,0], { radius: 0});
+  markers: L.Layer[] = [];
 
   async ngOnInit() {
     let latLong:LatLong = await getLocation();
     this.latitude = latLong.latitude;
     this.longitude = latLong.longitude;
+    console.log('user latlong', this.latitude, this.longitude);
+    this.placeService.getAllPlaces(null, null, null, null, null)
+      .subscribe({
+        next: res => {
+          this.places.set(res);
+          this.places().forEach(p => {
+            console.log('place latlong', p.latitude, p.longitude);
+            this.markers.push(L.marker([p.latitude, p.longitude]
+            //   , {
+            //   icon: L.icon({
+            //     ...L.Icon.Default.prototype.options,
+            //     iconUrl: 'assets/marker-icon.png',
+            //     iconRetinaUrl: 'assets/marker-icon-2x.png',
+            //     shadowUrl: 'assets/marker-shadow.png'
+            //   })
+            // }
+            ));
+          });
+          this.markers = [...this.markers];
+        },
+        error: err => console.error('could not retrieve nearby places for user', err)
+      });
 
-    this.options.center = latLng(this.latitude, this.longitude);
-    this.layer = circle([this.latitude, this.longitude], { radius: 50});
+    this.options = {
+      layers: [
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+          maxZoom: 18,
+          attribution: '...'
+        })
+      ],
+      zoom: 15,
+      center: L.latLng(this.latitude, this.longitude)
+    };
+    this.layer = L.circle([this.latitude, this.longitude], { radius: 50});
   }
 }
